257b890f34e737596a2618b674aa487898a2a250
Chindalath Traymany <chindalatht@gmail.com>
2020-07-22 05:04:24 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Customer
+  attr_accessor :name, :age
+  @@all = []
   
+  def initialize(name, age)
+    @name = name
+    @age = age
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+  
+  def meals
+    Meal.all.select { |meal| meal.customer == self}
+  end
+  
+  def waiters
+    Waiter.all.find { |waiter| waiter.meals.customer == self}
+  end
+  
+    
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
+require 'pry'
 class Waiter
+  attr_accessor :name, :experience
+  @@all = []
+
+  def initialize(name, experience)
+    @name = name
+    @experience = experience
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.waiter == self }
+  end
+
+
 
 end
